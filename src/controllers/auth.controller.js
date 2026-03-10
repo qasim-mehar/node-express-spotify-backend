3aa7665f598fc288f573dd205699696136f78cf7
Qasim <mianqasim065@gmail.com>
2026-03-10 19:47:17 +0500

    feat(auth): implement user login controller with jwt cookies

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -45,4 +45,33 @@ async function registerUser(req, res) {
   });
 }
 
-module.exports = { registerUser };
+async function loginUser(req, res) {
+  const { username, email, password } = req.body;
+
+  const user = await userModel.findOne({
+    $or: [{ email }, { username }],
+  });
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid credentials",
+    });
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+  );
+  res.cookie("token", token);
+  res.status(200).json({
+    message: "User login",
+  });
+}
+module.exports = { registerUser, loginUser };
