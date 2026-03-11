b93302fa6953b8f73aea91c5f9a2a34049b810fb
Qasim <mianqasim065@gmail.com>
2026-03-11 15:19:35 +0500

    feat(storage): implement imagekit music upload service

---
+++ src/services/storage.service.js
@@ -0,0 +1,15 @@
+const imageKit = require("@imagekit/nodejs");
+
+const imageKitClient = new imageKit({
+  privateKey: process.env["IMAGEKIT_PRIVATE_KEY"],
+});
+
+async function uploadFile(file) {
+  const result = await imageKitClient.files.upload({
+    file,
+    fileName: "music_" + Date.now(),
+    folder: "spotify-backend/music",
+  });
+  return result;
+}
+module.exports = uploadFile;
