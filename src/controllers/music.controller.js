59029d6801eb066ad4d5831ba92bbbe14a31d0ef
Qasim <mianqasim065@gmail.com>
2026-03-12 20:11:52 +0500

    refactor(api): decouple authorization and implement album creation

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -2,87 +2,47 @@ const musicModel = require("../models/music.model");
 const albumModel = require("../models/album.model");
 const jwt = require("jsonwebtoken");
 const { uploadFile } = require("../services/storage.service");
+const authMiddleware = require("../middleware/auth.middleware");
 
 async function createMusic(req, res) {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).json({
-      message: "Unauthorized",
-    });
-  }
+  const { title } = req.body;
+  //getting decoded value from middleware
+  const decoded = req.user;
+  const file = req.file;
+  const result = await uploadFile(file.buffer.toString("base64"));
 
-  try {
-    //IT WILL RETURN VALUES WHICH WE PUT IN TOKEN WHILE AUTHENTICATION, USER _id AND ROLE IN THIS SITUATION
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const music = await musicModel.create({
+    uri: result.url,
+    title,
+    artist: decoded.id,
+  });
 
-    if (decoded.role !== "artist") {
-      return res.status(403).json({
-        message: "Only artist can create music",
-      });
-    }
-    const { title } = req.body;
-    const file = req.file;
-    const result = await uploadFile(file.buffer.toString("base64"));
-
-    const music = await musicModel.create({
-      uri: result.url,
-      title,
-      artist: decoded.id,
-    });
-
-    res.status(201).json({
-      message: "Music upload successfully",
-      id: music._id,
-      uri: music.uri,
-      title: music.title,
-      artist: music.artist,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(401).json({
-      message: "Unauthorized",
-    });
-  }
+  res.status(201).json({
+    message: "Music upload successfully",
+    id: music._id,
+    uri: music.uri,
+    title: music.title,
+    artist: music.artist,
+  });
 }
 
 async function createAlbum(req, res) {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).json({
-      message: "Unauthorized",
-    });
-  }
+  //Creating an album
+  const { title, musics } = req.body;
+  //getting decoded value from middleware
+  const decoded = req.user;
+  const album = await albumModel.create({
+    title,
+    musics: musics,
+    artist: decoded.id,
+  });
 
-  try {
-    //Authorization
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
-    if (decoded.role !== "artist") {
-      return res.status(403).json({
-        message: "Only artist can upload an album",
-      });
-    }
-
-    //Creating an album
-    const { title, musics } = req.body;
-    const album = await albumModel.create({
-      title,
-      musics: musics,
-      artist: decoded.id,
-    });
-
-    res.status(201).json({
-      message: "Album created successfully",
-      id: album._id,
-      title: album.title,
-      musics: album.musics,
-      artist: album.artist,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(401).json({
-      message: "Unauthorized",
-    });
-  }
+  res.status(201).json({
+    message: "Album created successfully",
+    id: album._id,
+    title: album.title,
+    musics: album.musics,
+    artist: album.artist,
+  });
 }
 module.exports = { createMusic, createAlbum };
