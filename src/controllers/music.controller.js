54c45a5032421fc3b93fc128c6999138d5e7ba56
Qasim <mianqasim065@gmail.com>
2026-03-12 20:27:20 +0500

    feat(api): populate artist details in music fetch query

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -47,7 +47,7 @@ async function createAlbum(req, res) {
 }
 
 async function getAllMusics(req, res) {
-  const musics = await musicModel.find().populate("artist");
+  const musics = await musicModel.find().populate("artist", "username email");
   res.status(200).json({
     message: "Musics fetched succesfully",
     musics,
